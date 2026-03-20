4e9d7c2553ab8f049bcb354bd01e31e2b5b0991f
tundenewcodes <fasasipelumi1652@gmail.com>
2026-03-20 23:58:53 +0100

    save new user

--- src/app/user/providers/users.service.ts
+++ src/app/user/providers/users.service.ts
@@ -1,13 +1,31 @@
 import { forwardRef, Inject, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
 import { AuthService } from "src/app/auth/auth.service";
+import { User } from "../entities/user.entity";
+import { Repository } from "typeorm";
+import { CreateUserDto } from "../dto/create-user.dto";
 
 @Injectable()
 export class UserService {
   constructor(
     @Inject(forwardRef(() => AuthService))
-    private readonly authService: AuthService
+    private readonly authService: AuthService,
+    @InjectRepository(User) private userRepository: Repository<User>
   ) {}
 
+  async createUser(createUserDto: CreateUserDto) {
+    const existingUser = await this.userRepository.findOne({
+      where: { email: createUserDto.email }
+    });
+
+    if (existingUser) {
+      throw new Error("already exisitng");
+    }
+
+    let newUser = this.userRepository.create(createUserDto);
+    newUser = await this.userRepository.save(newUser);
+    return newUser;
+  }
   public findOneById(id: string) {
     const auth = this.authService.isAuth();
     console.log(auth);
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,11 +3,13 @@ import { forwardRef, Module } from "@nestjs/common";
 import { UserController } from "./user.controller";
 import { UserService } from "./providers/users.service";
 import { AuthModule } from "../auth/auth.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./entities/user.entity";
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
-  imports: [forwardRef(() => AuthModule)]
+  imports: [forwardRef(() => AuthModule), TypeOrmModule.forFeature([User])]
 })
 export class UserModule {}
